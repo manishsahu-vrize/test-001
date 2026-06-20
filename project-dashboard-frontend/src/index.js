612ce9a6242177b6137d30c31552ac2e7592af5f
Manish Kumar Sahu <154328866+manishsahu-vrize@users.noreply.github.com>
2026-06-20 13:11:42 +0530

    Dashboard implementation for the app.

---
+++ project-dashboard-frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProjectDashboard from './components/ProjectDashboard';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ProjectDashboard />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
